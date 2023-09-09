daac53b1a10d8c740584664386dc09fe4604cf5e
BatJosh46 <135491512+BatJosh46@users.noreply.github.com>
2023-09-09 15:13:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,30 @@ ctx.lineWidth = 3;
 ctx.strokeStyle = '#000000';
 ctx.font = '20px Arial';
 
+function loadImage() {
+    const input = document.getElementById('imageInput');
+    const file = input.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            const img = new Image();
+
+            img.onload = function() {
+                ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+            }
+
+            img.src = e.target.result;
+        }
+
+        reader.readAsDataURL(file);
+    }
+}
+
 function addText() {
     const text = document.getElementById('textInput').value;
-    ctx.fillText(text, 50, 50); // Adjust position as needed
+    ctx.fillText(text, 50, 50);
 }
 
 function clearCanvas() {
